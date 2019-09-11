a70c067662fb0144349b41ebd8b1bfea595bb689
Karl Martin T.Jensen <kama.teje@gmail.com>
2019-09-11 12:56:15 +0200

    tictactoeee jeg sliter litt

---
+++ tre på rad V1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace tre_på_rad_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.Location = new System.Drawing.Point(210, 265);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(64, 64);
+            this.btn00.TabIndex = 0;
+            this.btn00.UseVisualStyleBackColor = true;
+            this.btn00.Click += new System.EventHandler(this.btnBoard);
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(280, 265);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(64, 64);
+            this.btn10.TabIndex = 1;
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btnBoard);
+            // 
+            // btn20
+            // 
+            this.btn20.Location = new System.Drawing.Point(350, 265);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(64, 64);
+            this.btn20.TabIndex = 2;
+            this.btn20.UseVisualStyleBackColor = true;
+            this.btn20.Click += new System.EventHandler(this.btnBoard);
+            // 
+            // btn01
+            // 
+            this.btn01.Location = new System.Drawing.Point(210, 195);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(64, 64);
+            this.btn01.TabIndex = 3;
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.btnBoard);
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(280, 195);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(64, 64);
+            this.btn11.TabIndex = 4;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.btnBoard);
+            // 
+            // btn21
+            // 
+            this.btn21.Location = new System.Drawing.Point(350, 195);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(64, 64);
+            this.btn21.TabIndex = 5;
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.btnBoard);
+            // 
+            // btn02
+            // 
+            this.btn02.Location = new System.Drawing.Point(210, 125);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(64, 64);
+            this.btn02.TabIndex = 6;
+            this.btn02.UseVisualStyleBackColor = true;
+            this.btn02.Click += new System.EventHandler(this.btnBoard);
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(280, 125);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(64, 64);
+            this.btn12.TabIndex = 7;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.btnBoard);
+            // 
+            // btn22
+            // 
+            this.btn22.Location = new System.Drawing.Point(350, 125);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(64, 64);
+            this.btn22.TabIndex = 8;
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.btnBoard);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ tre på rad V1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tre_på_rad_V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] board = new string[3, 3];
+        string player = "X";
+        
+        
+        bool spiller1 = true; //Setter x
+        private void btnBoard(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+
+            
+            if (b.Text == "")
+            {
+                setBoard(b);
+                if (spiller1)
+                {
+                    b.Text = player;
+                    player = "O";
+
+                }
+                else
+                {
+                    b.Text = player;
+                    player = "X";
+                }
+                spiller1 = !spiller1;
+            }
+         if (checkWin())
+            {
+                label1.Text = "WIN";
+            }
+            
+        }
+        private void setBoard(Button btn)
+        {
+            int xpos = Convert.ToInt16(btn.Name[btn.Name.Length - 2]);
+            int ypos = Convert.ToInt16(btn.Name[btn.Name.Length - 1]);
+            board[xpos, ypos] = "player";
+        }
+        private bool checkWin()
+        {
+            if (checkHor() || checkVert())
+            {
+                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool checkHor()
+        {
+            for (int i = 0; 1 < 3; i++)
+            {
+                int count = 0;
+                for (int x = 0; x < 2; x++)
+                {
+                    if (board[x, i] == board[x + 1, i] && board[x, i] == player)
+                    {
+                        count++;
+                    }
+                    if (count == 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private bool checkVert()
+        {
+            for (int i = 0; 1 < 3; i++)
+            {
+                int count = 0;
+                for (int y = 0; y < 2; y++)
+                {
+                    if (board[i, y] == board[i, y + 1] && board[i, y] == player)
+                    {
+                        count++;
+                        if (count == 2)
+                        {
+                            return true;
+                        }
+                    }
+                    
+                }
+            }
+            return false;
+        }
+        
+    }
+}
